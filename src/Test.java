3c40486a064a9c5aaa2ce1f546c578faabb2caf4
lenovo <1049501781@qq.com>
2020-12-29 20:59:03 +0800

    让用户输入商品id，显示商品信息

--- src/Test.java
+++ src/Test.java
@@ -5,14 +5,14 @@ public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
         boolean bool = true;
         while (bool) {
-            System.out.println("请输入您的用户名");
+            System.out.println("请输入您的用户名：");
             Scanner sc = new Scanner(System.in);
             String username = sc.next();
-            System.out.println("您的用户名为" + username);
+            System.out.println("您的用户名为：" + username);
 
-            System.out.println("请输入您的密码");
+            System.out.println("请输入您的密码：");
             String password = sc.next();
-            System.out.println("您的密码为" + password);
+            System.out.println("您的密码为：" + password);
             //File file= new File("C:\\Users\\lenovo\\IdeaProjects\\ConsoleShop\\User.xls");
             InputStream inusers = Class.forName("Test").getResourceAsStream("/users.xlsx");
             InputStream inproduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
@@ -22,7 +22,7 @@ public class Test {
             User[] users = readUsersExcel.readExcel(inusers);
             for (int i = 0; i < users.length; i++) {
                 if (username.equals(users[i].getUsername().trim()) && password.equals(users[i].getPassword())) {
-                    System.out.println("登陆成功");
+                    System.out.println("登陆成功！");
                     /*读取商品信息*/
                     ReadProductExcel readProductExcel =new ReadProductExcel();
                     Product[] products=readProductExcel.getAllproducts(inproduct);
@@ -34,14 +34,14 @@ public class Test {
                         System.out.println("\t\t"+product.getProductpri());
                     }
                     /*根据id输出商品信息*/
-                    System.out.println("请输入商品id");
+                    System.out.println("请输入商品id：");
                     String pId = sc.next();
                     ReadProductExcel readProductExcel1 =new ReadProductExcel();
                     inproduct = null;
                     inproduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
                     Product product = readProductExcel1.getproductById(pId,inproduct);
                     if(product!=null){
-                        System.out.println("找到商品");
+                        System.out.println("已找到商品，并加入购物车！");
                         System.out.print("\t"+product.getProductid());
                         System.out.print("\t"+product.getProductname());
                         System.out.print("\t"+product.getProductdic());
@@ -50,7 +50,7 @@ public class Test {
                     bool = false;
                     break;
                 } else {
-                    System.out.println("登陆失败");
+                    System.out.println("登陆失败！请重新登录：");
                     break;
                 }
             }
