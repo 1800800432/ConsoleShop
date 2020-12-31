c52282cd3f0c0b694d295cd1780cbc8e5b922fc2
lenovo <1049501781@qq.com>
2020-12-31 20:25:39 +0800

    1、查看商品 2、继续购物

--- src/Test.java
+++ src/Test.java
@@ -34,18 +34,51 @@ public class Test {
                         System.out.println("\t\t"+product.getProductpri());
                     }
                     /*根据id输出商品信息*/
+                    int count=0;
+                    Product product2[]=new Product[3];
                     System.out.println("请输入商品id：");
                     String pId = sc.next();
                     ReadProductExcel readProductExcel1 =new ReadProductExcel();
                     inproduct = null;
                     inproduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
                     Product product = readProductExcel1.getproductById(pId,inproduct);
-                    if(product!=null){
-                        System.out.println("已找到商品，并加入购物车！");
-                        System.out.print("\t"+product.getProductid());
-                        System.out.print("\t"+product.getProductname());
-                        System.out.print("\t"+product.getProductdic());
-                        System.out.println("\t"+product.getProductpri());
+
+
+                    /*一次购物后的操作，查看购物车或继续购物*/
+                    boolean bool1=true;
+                    while (bool1) {
+                        if (product != null) {
+                            product2[count++] = product;
+                            System.out.println("已找到商品，并加入购物车！");
+                            System.out.println("请输入选项：");
+                            System.out.println("输入1，查看购物车，输入2，继续购物");
+                            int cho = sc.nextInt();
+                            if (cho == 1) {
+                                System.out.print("\t" + product2[count++]);
+                                System.out.print("\t" + product2[count]);
+                                System.out.print("\t" + product2[count]);
+                                System.out.println("\t" + product2[count]);
+                                bool1=false;
+                            } else if (cho == 2) {
+                                ReadProductExcel readProductExcel2 = new ReadProductExcel();
+                                inproduct = null;
+                                inproduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
+                                Product[] productes = readProductExcel2.getAllproducts(inproduct);
+                                for (Product product1 : productes) {
+                                    System.out.print("\t" + product1.getProductid());
+                                    System.out.print("\t" + product1.getProductname());
+                                    System.out.print("\t\t" + product1.getProductdic());
+                                    System.out.println("\t\t" + product1.getProductpri());
+                                    /*二次购买*/
+                                    System.out.println("请输入商品id：");
+                                    String pId1 = sc.next();
+                                    ReadProductExcel readProductExcel3 = new ReadProductExcel();
+                                    inproduct = null;
+                                    inproduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
+                                    product = readProductExcel3.getproductById(pId1, inproduct);
+                                }
+                            }
+                        }
                     }
                     bool = false;
                     break;
